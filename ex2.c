99a45fab62b24ef42c5dc8cf328473eed74b736c
guerreirof25-crypto <guerreirof25@gmail.com>
2025-11-02 20:08:24 -0300

    Atualizar o ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,11 @@
+long tamanho(const char *nome_arquivo) {
+    FILE *arq = fopen(nome_arquivo, "rb");
+    if (!arq) return -1;
 
+    long bytes = 0;
+    while (fgetc(arq) != EOF)
+        bytes++;
+
+    fclose(arq);
+    return bytes;
+}
